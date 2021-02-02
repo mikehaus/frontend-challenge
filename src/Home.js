1f7e0d986b8086f9c8fab62b293be3d6c5ad7237
mikehaus <mike.haus.design@gmail.com>
2021-02-02 16:36:09 -0700

    added styled components after sass not compiling (confirmed computer issue)

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import './Home.css';
 import { useFetchAllMovies } from './rest';
+import styled from 'styled-components';
 
 /**
  * You have the option to use either REST
@@ -10,30 +11,41 @@ import { useFetchAllMovies } from './rest';
  * Use `graphql/useAllMoviesQuery` instead for
  * GraphQL.
  **/
+
+const AllMovieList = (props) => {
+  const movies = props.movList;
+  
+  return (
+    <div>
+      <ol>
+        {movies.map(movie => (
+          <li key={movie.id}>
+            {movie.title}
+            <ul>
+              <li>Release Date: {movie.releaseDate}</li>
+              <li>Description: {movie.overview}</li>
+              <li>Average Vote: {movie.voteAverage}</li>
+              <li>Total Votes: {movie.voteCount}</li>
+              <li>Genres: {movie.genres.join(', ')}</li>
+            </ul>
+          </li>
+        ))}
+        </ol>
+    </div>
+  );
+}
+
+
 const Home = () => {
   const { data, loading } = useFetchAllMovies();
 
   return (
-    <div className="home-container">
+    <div>
       <h1>Popular Movies</h1>
-
       {loading ? (
         <div>Loading movies...</div>
       ) : (
-        <ol>
-          {data.map(movie => (
-            <li key={movie.id}>
-              {movie.title}
-              <ul>
-                <li>Release Date: {movie.releaseDate}</li>
-                <li>Description: {movie.overview}</li>
-                <li>Average Vote: {movie.voteAverage}</li>
-                <li>Total Votes: {movie.voteCount}</li>
-                <li>Genres: {movie.genres.join(', ')}</li>
-              </ul>
-            </li>
-          ))}
-        </ol>
+        <AllMovieList movList={data} />
       )}
     </div>
   );
